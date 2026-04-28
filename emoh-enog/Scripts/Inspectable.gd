3c5cee4c236aa1645a9369c32683a1ec0ef1d5db
Katie-H258 <katie.hardy258@gmail.com>
2026-04-28 02:55:42 -0400

    added more things
    added new plant sprites (gotten free from itch.io and spriters resource)
    added arrows to rooms
    added tables to greenhouse
    added code for moving between scenes, haven't been properly connected yet.

--- emoh-enog/Scripts/Inspectable.gd
+++ emoh-enog/Scripts/Inspectable.gd
@@ -14,20 +14,48 @@ func _process(_delta: float) -> void:
 
 
 func interact():
+	
+	##Handles taking player from place to place
 	if interaction_name == "ParlorToFoyer":
 		if Input.is_action_just_pressed("interact"):
 			get_tree().change_scene_to_file("res://Scenes/foyer.tscn")
 			#$"../../Player".position == 
+		
+	if interaction_name == "FoyerToParlor": 
+		if Input.is_action_just_pressed("interact"): 
+			get_tree().change_scene_to_file("res://Scenes/parlor.tscn")
+			#insert player position here
 	
+	if interaction_name == "LivingSpaceToFoyer": 
+		if Input.is_action_just_pressed("interact"): 
+			get_tree().change_scene_to_file("res://Scenes/foyer.tscn")
+			#insert player position here
+	
+	if interaction_name == "FoyerToLivingSpace": 
+		if Input.is_action_just_pressed("interact"): 
+			get_tree().change_scene_to_file("res://Scenes/living_space.tscn")
+			#insert player position here
+	
+	if interaction_name == "GreenhouseToFoyer": 
+		if Input.is_action_just_pressed("interact"): 
+			get_tree().change_scene_to_file("res://Scenes/foyer.tscn")
+			#insert player position here
+	
+	if interaction_name == "FoyerToGreenhouse": 
+		if Input.is_action_just_pressed("interact"): 
+			get_tree().change_scene_to_file("res://Scenes/Greenhouse.tscn")
+			#insert player position here 
+	
+	##Handles labels and text disappearing 
 	if Input.is_action_just_pressed("interact"): 
 		$"../../Player/Camera2D/TextBox/TextBoxColor".visible = true 
 		$"../../Player/Camera2D/InterestLabel".visible = false
 
+	##Handles interactions with objects
 	if interaction_name == "PoolBookArea":
 		print("a pool book!")
 		$"../../Player/Camera2D/TextBox/TextBoxColor/Dialogue".text = "There's a beginner's guide to pool here..."
-		
-		
+	
 	if interaction_name == "EvilPlant":
 		$Player/Camera2D/TextBox/TextBoxColor/Dialogue.text = "It tried to bite me..."
 		
@@ -37,5 +65,6 @@ func interact():
 	##Place key under rug in living space, add arrow to lampshade when turned on to point to key. 
 	##Make large picture by the bed an indicator to turn on the lamp. Bookshelf is a red herring. 
 	
+	##Handles text disappearing after interactions
 	await get_tree().create_timer(4).timeout
 	$"../../Player/Camera2D/TextBox".visible = false
